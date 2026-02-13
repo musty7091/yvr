2693f99dd18650cb4f20774b5b665978f69d79f7
musty7091 <m.mkaradeniz@icloud.com>
2026-02-13 16:52:15 +0200

    kaydet yarın devam

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
+import os
 from flask import Flask
-from datetime import timedelta
+from dotenv import load_dotenv # Yeni eklendi
 from models import db
-from utils import kurlari_sabitle
-
-# Blueprintleri içeri alıyoruz
-from routes.genel_isler import genel_bp
 from routes.musteri_isleri import musteri_bp
 from routes.tedarikci_isleri import tedarikci_bp
+from routes.genel_isler import genel_bp
 from routes.gider_isleri import gider_bp
 
-app = Flask(__name__)
-app.secret_key = 'yvr_ozel_sifre_123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yvr_veritabani.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+# .env dosyasındaki değişkenleri yükle
+load_dotenv()
 
-# Veritabanını uygulamaya bağlıyoruz
+app = Flask(__name__)
+
+# GÜVENLİK: Secret key artık dışarıdan alınıyor, yoksa varsayılan atanıyor
+app.secret_key = os.getenv('SECRET_KEY', 'cok_gizli_varsayilan_anahtar')
+
+# VERİTABANI: Yol artık dışarıdan yönetilebilir
+app.config['SQLALCHEMY_DATABASE_NAME'] = os.getenv('DATABASE_URL', 'sqlite:///instance/yvr_veritabani.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
-# Parçaları ana makineye bağlıyoruz
+# Blueprint Kayıtları
 app.register_blueprint(genel_bp)
-app.register_blueprint(musteri_bp)
-app.register_blueprint(tedarikci_bp)
-app.register_blueprint(gider_bp)
+app.register_blueprint(musteri_bp, url_prefix='/musteriler')
+app.register_blueprint(tedarikci_bp, url_prefix='/tedarikciler')
+app.register_blueprint(gider_bp, url_prefix='/giderler')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        kurlari_sabitle()
-    app.run(debug=True, host='0.0.0.0')
+    # DEBUG modu artık .env dosyasından kontrol ediliyor
+    debug_mode = os.getenv('DEBUG', 'False') == 'True'
+    app.run(debug=debug_mode)
